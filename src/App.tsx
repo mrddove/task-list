7ccf378a984dc956f317e78b43bdb91c0e06bbdf
Dove <codegeekdove@gmail.com>
2026-08-12 17:00:07 +0800

    add darkmode input element

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from 'react'
 import Header from './Components/Header'
 
+type Mode = 'light' | 'dark'
+
 function App() {
+  const [darkMode, setDarkMode] = useState<Mode>('light')
+
+  function handleTheme() {
+    setDarkMode(darkMode === 'light' ? 'dark' : 'light')
+  }
   return (
     <div className="taskApp">
-      <Header />
+      <input
+        type="checkbox"
+        checked={darkMode === 'dark'}
+        className="theme-toggle"
+        onChange={handleTheme}
+      />
+      <Header handleTheme={handleTheme} />
     </div>
   )
 }
