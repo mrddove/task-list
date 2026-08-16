ecb579ace1a3a619f0643cc62512a26f2dd7a786
Dove <codegeekdove@gmail.com>
2026-08-16 18:55:01 +0800

    Add the card component to TaskListItem

--- src/Components/TaskItem/index.tsx
+++ src/Components/TaskItem/index.tsx
@@ -5,10 +5,22 @@ import styles from './style.module.scss'
 
 type TaskItemProps = TaskDataType
 
-export default function TaskItem({ text }: TaskItemProps) {
+export default function TaskItem({ text, priority }: TaskItemProps) {
   return (
     <li className={styles.item}>
-      <Card>{text}</Card>
+      <Card>
+        <div className={styles.item__body}>
+          <p className={styles.item__text}>{text}</p>
+          <div className={styles.item__meta}>
+            <span
+              className={`${styles.item__tag} ${styles[`tag-priority-${priority}`]}`}
+            >
+              {priority}
+            </span>
+            <span className={`${styles.item__tag} tag-due`}>Due tomorrow</span>
+          </div>
+        </div>
+      </Card>
     </li>
   )
 }
