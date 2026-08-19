0466355915c13a60f2b260cfe7e1405d16c11682
Dove <codegeekdove@gmail.com>
2026-08-19 12:36:45 +0800

    add checkbox feature for taskdone state

--- src/Components/TaskItem/index.tsx
+++ src/Components/TaskItem/index.tsx
@@ -6,25 +6,61 @@ import styles from './style.module.scss'
 
 type TaskItemProps = TaskDataType
 
-export default function TaskItem({ title, priority, dueDate }: TaskItemProps) {
+export default function TaskItem({
+  title,
+  priority,
+  dueDate,
+  completed,
+}: TaskItemProps) {
   const dueClass = taskDueDate(dueDate)
 
   return (
     <li className={styles.item}>
       <Card>
+        {/* Checkbox */}
+        <label className={styles.item__label}>
+          <input type="checkbox" className={styles.item__checkbox} />
+          <span
+            className={[
+              styles.item__customCheckbox,
+              `${completed ? styles.checked : null}`,
+            ].join(' ')}
+            aria-hidden="true"
+          ></span>
+          {completed && (
+            <span className="sr-only">Mark "{title}" as complete</span>
+          )}
+        </label>
+        {/* Actual List */}
         <div className={styles.item__body}>
-          <p className={styles.item__text}>{title}</p>
+          <p
+            className={[
+              styles.item__text,
+              `${completed ? styles.completed : null}`,
+            ].join(' ')}
+          >
+            {title}
+          </p>
           <div className={styles.item__meta}>
             <span
               className={`${styles.item__tag} ${styles[`tag-priority-${priority}`]}`}
             >
               {priority}
             </span>
-            <span
-              className={`${styles.item__tag} ${styles[`tag-${dueClass}`]}`}
-            >
-              due {dueClass}
-            </span>
+
+            {completed ? (
+              <span
+                className={[styles.item__tag, styles['tag-done']].join(' ')}
+              >
+                Done
+              </span>
+            ) : (
+              <span
+                className={`${styles.item__tag} ${styles[`tag-${dueClass}`]}`}
+              >
+                due {dueClass}
+              </span>
+            )}
           </div>
         </div>
       </Card>
