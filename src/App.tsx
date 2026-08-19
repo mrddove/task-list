dc83c080733a490e0cf7e4b3d600057ba231f534
Dove <codegeekdove@gmail.com>
2026-08-20 00:00:20 +0800

    add handleAddTask function

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import FormTask from './Components/FormTask'
 import Header from './Components/Header'
 import ProgressBar from './Components/ProgressBar'
 import TaskList from './Components/TaskList'
+import { type TaskDataType } from './assets/taskData'
 
 type Mode = 'light' | 'dark'
 
 function App() {
   const [darkMode, setDarkMode] = useState<Mode>('light')
 
+  const [taskData, setTaskData] = useState<TaskDataType[]>([])
+
   function handleTheme() {
     setDarkMode(darkMode === 'light' ? 'dark' : 'light')
   }
+
+  function handleAddTask(item: TaskDataType) {
+    setTaskData((prevTaskData) => [...prevTaskData, item])
+  }
+
   return (
     <div className="container" data-theme={darkMode}>
       <input
@@ -24,8 +32,8 @@ function App() {
       <div className="taskApp">
         <Header handleTheme={handleTheme} />
         <ProgressBar />
-        <FormTask />
-        <TaskList />
+        <FormTask onAddTask={handleAddTask} />
+        <TaskList taskData={taskData} />
       </div>
     </div>
   )
