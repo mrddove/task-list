2790f5b978f010e2a239203e1d34e206dff55e09
Dove <codegeekdove@gmail.com>
2026-08-19 23:59:34 +0800

    add dynamic input attributes

--- src/Components/FormTask/SelectPriority/index.tsx
+++ src/Components/FormTask/SelectPriority/index.tsx
@@ -1,15 +1,16 @@
 import { MdKeyboardArrowDown } from 'react-icons/md'
 
+import type { InputProps } from '../types'
 import { options } from './optionsData'
 import styles from './style.module.scss'
 
-export default function SelectPriority() {
+export default function SelectPriority({ name, label, id }: InputProps) {
   return (
     <div className={`${styles.priority} wrap`}>
       <label htmlFor="priority-select" className="sr-only">
-        Priority
+        {label}
       </label>
-      <select id="priority-select" name="priority" className="priority__select">
+      <select id={id} name={name} className="priority__select">
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.value}
