416eb0844a0f08010cb85b7afd5c5ba6e068ed74
Dove <codegeekdove@gmail.com>
2026-08-27 21:25:12 +0800

    Add DueDateInput component

---
+++ src/Components/DueDateInput/index.tsx
@@ -0,0 +1,31 @@
+import type { InputProps } from '../../types'
+import styles from './style.module.scss'
+
+import { FiCalendar } from 'react-icons/fi'
+
+export default function DueDateInput({
+  name,
+  label,
+  id,
+  type,
+  defaultValue,
+}: InputProps) {
+  return (
+    <div className={`${styles.date} wrap`}>
+      <label htmlFor="due-date-input" className="sr-only">
+        {label}
+      </label>
+      <input
+        defaultValue={defaultValue}
+        type={type}
+        id={id}
+        name={name}
+        className="date__input"
+      />
+
+      <div className={styles.date__icon}>
+        <FiCalendar />
+      </div>
+    </div>
+  )
+}
