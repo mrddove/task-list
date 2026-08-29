1e0679d97351595769cb714cc02e1e94553f8e87
Dove <codegeekdove@gmail.com>
2026-08-29 16:23:07 +0800

    Add date feature that prevent users from selecting past dates

--- src/Components/DueDateInput/index.tsx
+++ src/Components/DueDateInput/index.tsx
@@ -10,6 +10,8 @@ export default function DueDateInput({
   type,
   defaultValue,
 }: InputProps) {
+  const today = new Date().toISOString().split('T')[0]
+
   return (
     <div className={`${styles.date} wrap`}>
       <label htmlFor="due-date-input" className="sr-only">
@@ -21,6 +23,7 @@ export default function DueDateInput({
         id={id}
         name={name}
         className="date__input"
+        min={today}
       />
 
       <div className={styles.date__icon}>
